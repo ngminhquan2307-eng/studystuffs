e1b29378ca36309b7bee7ed15382873de1977558
ngminhquan2307-eng <ngminhquan2307@gmail.com>
2025-09-16 13:51:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function sayHello() {
-  alert("Hello from JavaScript!");
+function login() {
+  const username = document.getElementById("username").value;
+  if(username) {
+    localStorage.setItem("user",username);
+    showPage();
+  }
 }
+
+function logout() {
+  localStorage.removeItem("user");
+  showPage();
+}
+
+function showPage() {
+  const user = localStorage.getItem("user");
+  if (user) {
+    document.getElementById("guest").style.display = "none";
+    document.getElementById("loggedIn").style.display = "block";
+    document.getElementById("user").innerText = user;
+  } else {
+    document.getElementById("guest").style.display = "none";
+    document.getElementById("loggedIn").style.display = "block";
+  }
+}
+
+showPage();
